9937d4ba0ccb4d82775b92c5a04df9213ab9e456
NasKinloch <51344359+NasKinloch@users.noreply.github.com>
2021-05-20 10:14:07 +1200

    javaScript cart loading data
    finishing off this last part of the cart then it will be finished

--- shopping.js
+++ shopping.js
@@ -1,9 +1,51 @@
   var items = [
-        ['images/item 1.png', 29.99, 'product 1'],
-        ['images/item 2.png', 19.99, 'product 2'],
-        ['images/item 3.png', 9.99, 'product 3'],
-        ['images/item 4.png', 39.99, 'product 4']
+        ['images/item1.png', 29.99, 'product 1'],
+        ['images/item2.png', 19.99, 'product 2'],
+        ['images/item3.png', 9.99, 'product 3'],
+        ['images/item4.png', 39.99, 'product 4']
     ];
+
+  var cartItems = [];
+
+  function run() { // loading products on cart page
+      var main = document.getElementById('products');
+
+      //all elements to be added
+      for (var i = 0; i < items.length; i++) {
+
+
+          var ele = document.createElement('li');
+          var pic = document.createElement('img');
+          var price = document.createElement('h1');
+          var desc = document.createElement('h2');
+          var add = document.createElement('button');
+          var inputBox = document.createElement('input');
+
+
+          //push elements into html
+          main.appendChild(ele);
+          ele.appendChild(pic);
+          ele.appendChild(price);
+          ele.appendChild(desc);
+          ele.appendChild(add);
+          ele.appendChild(inputBox);
+
+          //edit pushed elements info from array
+
+          pic.src = items[i][0];
+          price.innerHTML = '$' + items[i][1];
+          desc.innerHTML = items[i][2];
+
+          add.innerHTML = 'Add';
+          add.dataset.cartIndex = i;
+          add.addEventListener('click', adding, false);
+
+          inputBox.value = 1;
+          inputBox.type = 'number';
+          inputBox.setAttribute('id', 'input' + i);
+      }
+  }
+
 
   function adding(event) {
       const NUM = event.currentTarget.dataset.cartIndex;
@@ -17,15 +59,15 @@
   var totalItems = 0;
 
   function updateCart() {
-      var itemCount = document.getElementById('itemCount');
+      var itemCount = document.getElementById('totalItems');
 
       totalItems = 0;
       for (var i = 0; i < cartItems.length; i++) {
           totalItems += cartItems[i][1];
       }
-      
-      
-      
+
+      window.sessionStorage.setItem('cartItems', JSON.stringify(cartItems));
+
       itemCount.innerHTML = totalItems;
   }
 
@@ -33,6 +75,13 @@
       var main = document.getElementById('cartProducts');
 
       //all elements to be added
+      
+      var data = sessionStorage.getItem('cartItems');
+      data =JSON.parse(data);
+      
+      cartItems = data;
+      
+      
       for (var i = 0; i < items.length; i++) {
 
 
@@ -71,4 +120,3 @@
   function deleteMe() {
       alert('gone')
   }
-
